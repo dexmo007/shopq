41dd44993d841739ea35167eefff4f1b0f4156e3
Henrik Drefs <henrik.drefs@outlook.de>
2020-03-21 22:45:06 +0100

    fetch place details

--- src/views/Market.vue
+++ src/views/Market.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="market">
     <h1>Laden #{{id}}</h1>
-    <div id="primary-interaction" v-if="!inQ">
-      <button id="joinQ-btn" @click="joinQ()">virtuell anstellen</button>
+    <div
+      id="primary-interaction"
+      v-if="!inQ"
+    >
+      <button
+        id="joinQ-btn"
+        @click="joinQ()"
+      >virtuell anstellen</button>
       <span v-if="WaitingTimeStr">geschätzte Wartezeit: <b>{{WaitingTimeStr}}</b></span>
     </div>
     <div v-else>
       <span>Sie sind der <b>{{positionInQ}}.</b> in der Schlange.</span>
-      <button id="quitQ-btn" @click="quitQ()">Schlange verlassen</button>
+      <button
+        id="quitQ-btn"
+        @click="quitQ()"
+      >Schlange verlassen</button>
     </div>
+
+    <div class="info">
+
+      {{!placeDetails ? 'Loading...' : placeDetails}}
+    </div>
+
     <div class="card">
       <h2>Auslastung am Vortag</h2>
       <p></p>
@@ -16,51 +31,65 @@
   </div>
 </template>
 <script>
- export default {
-    name: 'market',
-    props: {
-      id: String
+import { gmapApi } from "vue2-google-maps";
+
+export default {
+  name: "market",
+  props: {
+    id: String
+  },
+  computed: {
+    google: gmapApi,
+    WaitingTimeStr() {
+      return "12min";
     },
-   created() {
-      //console.log(this.id);
-   },
-   computed: {
-     WaitingTimeStr () {
-        return "12min";
-     },
-     positionInQ (){
-       return 4;
-     }
-
-   },
-   data() {
-     return {
-       inQ: false
-     }
-   },
-   methods: {
-     joinQ(){
-       this.inQ = true;
-     },
-     quitQ(){
-       this.inQ = false;
-     }
-   }
-
- }
+    positionInQ() {
+      return 4;
+    }
+  },
+  data() {
+    return {
+      placeDetails: null,
+      inQ: false
+    };
+  },
+  methods: {
+    joinQ() {
+      this.inQ = true;
+    },
+    quitQ() {
+      this.inQ = false;
+    }
+  },
+  async created() {
+    await this.$gmapApiPromiseLazy();
+    const google = this.google;
+    const service = new google.maps.places.PlacesService(
+      document.createElement("div")
+    );
+    service.getDetails(
+      {
+        placeId: this.id
+      },
+      placeDetails => {
+        this.placeDetails = placeDetails;
+      }
+    );
+  }
+};
 </script>
 <style type="text/css" scoped>
-  #joinQ-btn{
-    background-color: #64c7a6;
-    width: 100%;
-    max-width: 480px;
-    margin: 0 auto;
-  }
-  #quitQ-btn{
-    background-color: transparent;
-    color: #dd363a;
-    width: 100%;
-    max-width: 480px;
-    margin: 0 auto;
-  }
+#joinQ-btn {
+  background-color: #64c7a6;
+  width: 100%;
+  max-width: 480px;
+  margin: 0 auto;
+}
+#quitQ-btn {
+  background-color: transparent;
+  color: #dd363a;
+  width: 100%;
+  max-width: 480px;
+  margin: 0 auto;
+}
 </style>
