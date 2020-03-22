dc2de7f2cee97a6fdf405801f0cc1a51acd10abf
Henrik Drefs <henrik.drefs@outlook.de>
2020-03-23 00:17:08 +0100

    stop stream on destroy

--- src/components/QRCodeScanner.vue
+++ src/components/QRCodeScanner.vue
@@ -16,6 +16,9 @@ import QrScanner from "qr-scanner";
 QrScanner.WORKER_PATH = "./assets/qr-scanner-worker.min.js";
 
 export default {
+  data() {
+    return { stream: null };
+  },
   mounted() {
     // new QrScanner(this.$refs.qrscan, result =>
     //   console.log("decoded qr code:", result)
@@ -25,8 +28,9 @@ export default {
     };
     navigator.mediaDevices.getUserMedia(constraints).then(stream => {
       this.$refs.qrscan.srcObject = stream;
+      this.stream = stream;
     });
-  }
+  },
   // mounted() {
   //   let scanner = new Instascan.Scanner({
   //     video: this.$refs.qrscan,
@@ -47,5 +51,12 @@ export default {
   //       console.error(e);
   //     });
   // }
+  beforeDestroy() {
+    if (this.stream) {
+      this.stream.getTracks().forEach(track => {
+        track.stop();
+      });
+    }
+  }
 };
 </script>
