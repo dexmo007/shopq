3dcff177240a1054ca6108059f58e49e2e3063d8
Henrik Drefs <henrik.drefs@outlook.de>
2020-03-23 00:17:19 +0100

    removed console.logs

--- src/components/QRCode.vue
+++ src/components/QRCode.vue
@@ -14,10 +14,8 @@ export default {
   name: "QRCode",
   props: ["text"],
   mounted() {
-    console.log(this.text);
     QRCode.toCanvas(this.$refs.canvas, this.text, function(error) {
       if (error) console.error(error);
-      console.log("success!");
     });
   }
 };
