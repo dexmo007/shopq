da54523d14853bc88c5092018bfbce923303b9ef
PunktSonstNichts <t.meyer-arlt@ostfalia.de>
2020-04-05 14:37:12 +0200

    der Anfang einer ProgressBar.vue

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,79 @@
+<template>
+    <div id="progress">
+        <div id="limit-wrapper">
+            <span :style="{width: underLimitWidth}"/>
+            <span id="limit">{{this.threshold}}</span>
+            <span :style="{width: overLimitWidth}"/>
+        </div>
+        <div id="progressbar-wrapper">
+            <div id="progressbar" :class="{alone: !isOverflow}">
+                <div id="under-limit" :style="{width: underLimitWidth}">{{this.count}}</div>
+            </div>
+            <div id="progressbar-overflow" v-if="isOverflow" :style="{width: overLimitWidth}">
+                <div id="over-limit"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProgressBar.vue",
+        props: {
+            threshold: {
+                type: Number,
+                default: 20
+            },
+            count: {
+                type: Number,
+            }
+        },
+        computed: {
+            isOverflow(){
+                return this.count > this.threshold;
+            },
+            underLimitWidth(){
+                return this.isOverflow ? "100%" : this.count/this.threshold * 100 + "%";
+            },
+            overLimitWidth(){
+                return !this.isOverflow ? "0%" : this.count-this.threshold * 12 + "px";
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+    #progress{
+        width: 100%;
+    }
+#progressbar-wrapper{
+    width: 100%;
+    height: 32px;
+    display: flex;
+}
+#progressbar{
+    border-style:solid;
+    border-width: 1px 0 1px 1px;
+    flex: 1;
+    overflow: hidden;
+    border-radius: 4px 0 0 4px;
+}
+#progressbar.alone{
+    border-radius: 5px;
+}
+#progressbar-overflow{
+    border-style: dotted;
+    border-width: 1px 1px 1px 0;
+    flex: 1;
+}
+#under-limit{
+    background-color: #64c7a6;
+    height: 30px;
+}
+#over-limit{
+    background-color: #dd363a;
+    height: 30px;
+    width: 100%;
+}
+</style>
