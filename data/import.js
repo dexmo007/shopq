0e12ea839cc3d7a4918c6783f9abf46255ad3f68
Henrik Drefs <henrik.drefs@outlook.de>
2020-04-22 13:55:32 +0200

    separate dev and prod env

--- data/import.js
+++ data/import.js
@@ -5,22 +5,22 @@ const path = require('path');
 
 const app = admin.initializeApp();
 
-const load = name =>
+const load = (name) =>
   JSON.parse(
     fs.readFileSync(path.resolve(__dirname, name), { encoding: 'utf8' })
   );
 
 const data = {
   groceries: load('groceries.json'),
-  vegetables: load('vegetables.json')
+  vegetables: load('vegetables.json'),
 };
 
 const items = Object.entries(data).map(([type, words]) => [
   type,
-  words.map(word => ({
+  words.map((word) => ({
     word,
-    rng: Math.random()
-  }))
+    rng: Math.random(),
+  })),
 ]);
 
 const col = app
@@ -29,11 +29,11 @@ const col = app
   .doc('de');
 
 for (const [type, words] of items) {
-  Promise.all(words.map(word => col.collection(type).add(word)))
-    .then(res => {
+  Promise.all(words.map((word) => col.collection(type).add(word)))
+    .then((res) => {
       console.log(
         `Successfully written ${res.length} words to list de/${type}`
       );
     })
-    .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 }
--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/firestore';
 
 const config = {
   apiKey: process.env.VUE_APP_GOOGLE_API_KEY,
-  authDomain: 'shopq-271815.firebaseapp.com',
-  databaseURL: 'https://shopq-271815.firebaseio.com',
-  projectId: 'shopq-271815',
-  storageBucket: 'shopq-271815.appspot.com',
-  messagingSenderId: '169605879088',
-  appId: '1:169605879088:web:9217f73b587e401582a4f5',
-  measurementId: 'G-EDF2ZX62M0',
+  ...JSON.parse(process.env.VUE_APP_FIREBASE_CONFIG),
 };
 
 firebase.initializeApp(config);
