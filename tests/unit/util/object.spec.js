4c119c61dd31b19f8c36b1d129305e07f85894fb
Henrik Drefs <henrik.drefs@outlook.de>
2020-05-02 12:37:41 +0200

    fixed test

--- tests/unit/util/object.spec.js
+++ tests/unit/util/object.spec.js
@@ -16,6 +16,6 @@ describe('util/object/changed', () => {
     [undefined, ''],
     [null, ''],
   ])('should be false for changed(%s, %s)', (left, right) => {
-    expect(changed(left, right)).toBe(true);
+    expect(changed(left, right)).toBe(false);
   });
 });
